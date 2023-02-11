e2d19db14819d398f5c180fbb5179fc5239491e9
hojiii <shimn3060@naver.com>
2023-02-11 14:41:55 +0900

    기본틀 입니다.

--- app.py
+++ app.py
@@ -3,8 +3,28 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 import certifi
+import jwt
+import hashlib
+import datetime
 
 ca = certifi.where()
-
-client = MongoClient('mongodb+srv://<username>:<password>@cluster0.wpr6ivo.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.0x2me9v.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/index')
+def main():
+    return render_template('index.html')
+
+@app.route('/sign_up')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5004, debug=True)
