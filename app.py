392837e73df1c1d443c35883b0660cfec837562a
hojiii <shimn3060@naver.com>
2023-02-11 23:39:53 +0900

    회원가입 js ,로그인js ,app 추가 /로그인 페이지, 회원가입 html 추가

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ import hashlib
 import datetime
 
 ca = certifi.where()
-client = MongoClient('mongodb+srv://test:sparta@cluster0.0x2me9v.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.qli9s79.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
+
+
 
 @app.route('/')
 def home():
@@ -23,8 +25,28 @@ def main():
     return render_template('index.html')
 
 @app.route('/sign_up')
-def signup():
-    return render_template('signup.html')
+def sign_up():
+    return render_template('sign_up.html')
+
+
+@app.route('/api/signup', methods=['POST'])
+def api_signup():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    pw_pw_receive = request.form['pw_pw_give']
+    nickname_receive = request.form['nickname_give']
+
+    check = db.login.find_one({'id': id_receive})
+
+    if check is not None:
+        return jsonify({'msg': '이미 존재하는 아이디입니다 !'})
+    elif pw_receive != pw_pw_receive:
+        return jsonify({'msg': '패스워드가 일치하지 않습니다 !'})
+    else:
+        pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+        db.login.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
+
+        return jsonify({'result': 'success', 'msg': '회원가입 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5004, debug=True)
